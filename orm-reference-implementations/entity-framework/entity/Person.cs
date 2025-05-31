2bb97c15534668fa968e7fad935030e0b7efcf5d
Bernd Stainer <bernd@stainer.net>
2025-05-31 10:30:15 +0200

    started documentation update

--- orm-reference-implementations/entity-framework/entity/Person.cs
+++ orm-reference-implementations/entity-framework/entity/Person.cs
@@ -5,7 +5,6 @@ namespace university.entity
     [Table("person")]
     public class Person : NamedElement
     {
-        //can't use ComplexType in Inheritance, need to manually inherit all attributes
         [Column(TypeName = "Varchar(255)")]
         public string? Street { get; set; }
 
